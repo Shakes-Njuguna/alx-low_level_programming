641fbbe3737e1ead029b58fc8473d67e851e3176
Shakes-Njuguna <ngamaunjuguna@gmail.com>
2023-03-16 13:18:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stidio.h>
+
+/**
+ * main -this is the main function
+ *
+ * Return -always 0
+ * /
+ 
+ int main(void)
+{
+	put ("\"programming is like building a multilingual puzzle);
+	return (0);
+}
