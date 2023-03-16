2e74d94330dfd45afce4acc26d8bbcce0532e742
Shakes-Njuguna <ngamaunjuguna@gmail.com>
2023-03-16 14:03:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -this is the main function 
+ *
+ * Rerurn: 0 when successful
+ */
+
+int main(viod)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,");
+	return (0)
+}
+
