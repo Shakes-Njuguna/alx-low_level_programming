71857f6a7abcb5987a0a180f6c21987b5a62a9ac
Shakes-Njuguna <ngamaunjuguna@gmail.com>
2023-03-16 13:35:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  *
  * Return: always 0
  */
-int main(void)
-	puts("\"programming is like building a multilingual puzzle);
+int main(void);
+{
+	puts("\"programming is like building a multilingual puzzle");
 	return (0)
 }
