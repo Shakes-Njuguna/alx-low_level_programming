1930e9e520eb4e8fd8ac76bf9442905c019231bb
Shakes-Njuguna <ngamaunjuguna@gmail.com>
2023-03-19 23:32:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - mai function
+ * Return: return always (0) (success0
+ */
+
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+
+}
