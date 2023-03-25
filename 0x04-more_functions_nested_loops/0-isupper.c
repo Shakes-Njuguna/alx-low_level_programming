3b9b5ce99553a7efa1492f0fe1b4864b92c0d4aa
Shakes-Njuguna <ngamaunjuguna@gmail.com>
2023-03-25 02:34:18 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - Check if a letter is upper
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _is upper(int x)
+int _is upper(int c)
 
 {
 
-	if (x >= 65 && x <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 	return (1);
 	}
